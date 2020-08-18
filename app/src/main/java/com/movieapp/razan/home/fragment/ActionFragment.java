ee25405de5031b7eec11f2e2a587d1318eb11e0a
razan <razang@sssit.net>
2020-08-18 15:44:28 +0300

    pager

--- app/src/main/java/com/movieapp/razan/home/fragment/ActionFragment.java
+++ app/src/main/java/com/movieapp/razan/home/fragment/ActionFragment.java
@@ -3,8 +3,7 @@ package com.movieapp.razan.home.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +13,12 @@ import com.movieapp.razan.R;
 
 public class ActionFragment extends Fragment {
 
-    private static final int NUM_PAGES = 5;
-    private ViewPager mPager;
+private RecyclerView recyclerView;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action, container, false);
+       View v=inflater.inflate(R.layout.fragment_action, container, false);
+       recyclerView=v.findViewById(R.id.recyclerView_moovies);
+    return v;
     }
 }
